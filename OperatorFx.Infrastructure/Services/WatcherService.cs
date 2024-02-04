c1d6a67d061d1ca2125dbfaef2eac26bd07bb947
Simon Lissack <lissack.simon@gmail.com>
2024-02-04 15:17:51 +0000

    Bones of the watcher service

--- OperatorFx.Infrastructure/Services/WatcherService.cs
+++ OperatorFx.Infrastructure/Services/WatcherService.cs
@@ -1,4 +1,6 @@
-﻿using k8s;
+﻿using System.Reflection;
+using k8s;
+using k8s.Models;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
@@ -7,9 +9,10 @@ using OperatorFx.Infrastructure.Configuration;
 
 namespace OperatorFx.Infrastructure.Services;
 
-public class WatcherService<T>(ILogger<WatcherService<T>> logger, IOptions<HostingOptions> hostingOptions, IKubernetes kubernetes) : BackgroundService where T : CustomResource
+public class WatcherService<T>(ILogger<WatcherService<T>> logger, IOptions<HostingOptions> hostingOptions, IKubernetes kubernetes) : BackgroundService
+    where T : CustomResource
 {
-    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         logger.LogInformation("starting watcher service for resource type {ResourceType}", typeof(T).Name);
 
@@ -18,6 +21,23 @@ public class WatcherService<T>(ILogger<WatcherService<T>> logger, IOptions<Hosti
             logger.LogInformation("Installing Custom Resource Definition");
         }
 
-        throw new NotImplementedException();
+        var k8sAttributes = typeof(T).GetCustomAttribute<KubernetesEntityAttribute>()!;
+
+        var response = await kubernetes.CustomObjects.ListClusterCustomObjectWithHttpMessagesAsync<T>(
+            k8sAttributes.Group,
+            k8sAttributes.ApiVersion,
+            k8sAttributes.PluralName,
+            watch: true,
+            cancellationToken: stoppingToken
+        );
+
+        response.Watch(OnEventReceived);
+
+        logger.LogInformation("Watcher service for resource type {ResourceType} stopping", typeof(T).Name);
     }
+
+    public Action<WatchEventType, T> OnEventReceived = (e, t) =>
+    {
+        logger.LogInformation("Event {Event} received for resource {ResourceType}", e, typeof(T).Name);
+    };
 }

89c14a6494b336779b3bfa3c0870595e6b6dae2b
Simon Lissack <lissack.simon@gmail.com>
2024-02-04 15:17:06 +0000

    Add Status to CustomResource

--- OperatorFx.Domain/Models/CustomResource.cs
+++ OperatorFx.Domain/Models/CustomResource.cs
@@ -6,4 +6,10 @@ namespace OperatorFx.Domain.Models;
 public class CustomResource : KubernetesObject, IKubernetesObject<V1ObjectMeta>
 {
     public V1ObjectMeta Metadata { get; set; } = new();
+    public V1CustomResourceStatus Status { get; set; } = new();
+}
+
+public class V1CustomResourceStatus
+{
+    List<V1Condition> Conditions { get; set; } = new();
 }

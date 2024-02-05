491a815f9a390835c09dcad1e13f5dfd827bf24f
Simon Lissack <lissack.simon@gmail.com>
2024-02-05 09:08:23 +0000

    event -> notification

---
+++ OperatorFx.Infrastructure/Eventing/KubernetesWatchNotification.cs
@@ -0,0 +1,11 @@
+﻿using k8s;
+using MediatR;
+using OperatorFx.Domain.Models;
+
+namespace OperatorFx.Infrastructure.Eventing;
+
+public class KubernetesWatchNotification<T> : INotification where T : CustomResource
+{
+    public WatchEventType Type { get; set; }
+    public T Resource { get; set; } = null!;
+}
